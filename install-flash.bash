14f59c3b170cb46304cf1135cbe97a75d2a80b82
ebeale <ebeale@computezero.evector>
2018-12-04 14:10:36 -0500

    auto commited on: 2018-12-04 19:10:36 [UTC]

--- install-flash.bash
+++ install-flash.bash
@@ -1,16 +1,40 @@
 #!/bin/bash
 
-#sudo dnf install \
-     http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+# Check if root
+if [[ "$EUID" != 0 ]]; then
+    printf "Please run as root!"
+    printf "\n"
+    exit 1
+fi
 
-#dnf provides */libpepflashplayer.so
+# Add official Flash repo
+dnf install -y "http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm"
 
-#sudo dnf install flash-player-ppapi
+# Refresh repos and upgrade
+dnf upgrade --refresh -y
 
-sudo ln -s \
-     /usr/lib64/flash-plugin/libpepflashplayer.so \
-     /usr/lib64/chromium-browser/PepperFlash/libpepflashplayer.so
+# Install depends for Pepper Flash
+dnf provides -y */libpepflashplayer.so
 
-sudo ln -s \
-     /usr/lib64/flash-plugin/manifest.json \
-     /usr/lib64/chromium-browser/PepperFlash/manifest.json
+# Install Pepper Flash
+dnf install -y "flash-player-ppapi"
+
+link-pepper-flash() {
+    # Symlink required files into Chromium dirs
+    ln -s \
+       /usr/lib64/flash-plugin/libpepflashplayer.so \
+       /usr/lib64/chromium-browser/PepperFlash/libpepflashplayer.so
+
+    ln -s \
+       /usr/lib64/flash-plugin/manifest.json \
+       /usr/lib64/chromium-browser/PepperFlash/manifest.json
+}
+
+# Install Chromium if not installed
+if $(which chromium-browser >/dev/null 2>&1); then
+    dnf install -y chromium
+fi
+
+# Provide Pepper Flash to Chromium
+link-pepper-flash
+    
