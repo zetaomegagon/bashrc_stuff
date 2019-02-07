5b06348584f507eb5c45393e816236d2ea50530a
Elijah Beale <elijahthomasbeale+git@gmail.com>
2019-02-07 09:55:36 -0500

    auto commited on: 2019-02-07 14:55:36 [UTC]

--- sshproxy.sh
+++ sshproxy.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-/usr/bin/ssh -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3" -D 8080 -qCN zomegagon@50.116.63.65
+
+# Start an ssh proxy to my linode in the background
+
+# functions
+start_proxy(){
+    while : ; do
+	if pgrep -a ssh | grep -q zomegagon; then
+	    continue
+	else
+	    /usr/bin/ssh -o "ServerAliveInterval 10" \
+			 -o "ServerAliveCountMax 3" \
+			 -D 8080 -qCN zomegagon@50.116.63.65
+	fi
+    done >/dev/null 2>&1
+}
+
+# main
+start_proxy &
+    
+    
