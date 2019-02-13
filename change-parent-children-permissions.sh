5cca22f6b4db65e9c3370032b288bf9422006410
ebeale <ebeale@computezero.evector>
2019-02-13 16:39:02 -0500

    auto commited on: 2019-02-13 21:39:02 [UTC]

--- change-parent-children-permissions.sh
+++ change-parent-children-permissions.sh
@@ -2,43 +2,92 @@
 
 # Goal: Make all objects in "Data Center Drive rclone" unsearcheable
 
-# Types
-# - folders
-# - forms
-# - document
-# - drawings
-# - presentation
-# - spreadsheets
-# - file
+#    1. get all objects
+#    2. strip all but url
+#    3. url types have standard form per type (listed below): https://aa/bb/cc vs https://one/two/three.
+#       form will determine the field the object ID is in. Sort lists by form and put somewhere for inspection.
+#    4. Make sure everything matches: (all urls) = (folders + forms + ...)
+#    5. get IDs from urls using standard url forms
+#    6. determine "Data Center Drive rclone" ID (from technology acct)
+#    7. GAM: utilize "<object_id> in <parent_id>" where parent_id is ID of "Data Center Drive rclone"
+
+#    Types for pattern matching
+
+#    - folders      = 4th field
+#    - forms        = 7th field
+#    - document     = 5th field
+#    - drawings     = 5th field
+#    - presentation = 5th field
+#    - spreadsheets = 5th field
+#    - file	    = 4th field
 
 #@ variables
-_dir="~/.tps/TPS-Scratch/GAM-Scripts"
+_dir="/home/ebeale/TPS-Scratch/GAM-Scripts/change-permissions"
 _filelist="$_dir/all-tech-files.txt"
 _urllist="$_dir/all-tech-urls.txt"
+tee="tee -a"
 
 #@ functions
 grep() { /usr/bin/grep "$@"; }
 
+get_line_count() { wc -l "$@" | awk '{ print $2 }'; }
+
 #@ main
+mkdir -p "$_dir" || :
+cd "$_dir" || exit
 
-# 1. get all objects
-gam user technology show file list > $_filelist
+parallel echo "" > {} ::: *
 
-# 2. strip all but url
-grep -o "https://.*" $_filelist > $_urllist
+{ printf   "%s\n" "Working Dir:     $_dir"\
+  ; printf "%s\n" "Current Dir:     $(pwd)"\
+  ; printf "%s\n" "Drive File list: $_filelist"\
+  ; printf "%s\n" "Object URLS:     $_urllist"; } >> $_dir/vars.txt
 
-# 3. url types have standard form per type: https://aa/bb/cc vs https://one/two/three
-_types=( 'folders' 'forms' 'documents' 'drawings' 'presentation' 'spreadsheets' 'file')
+declare -a _types
+_types=( 'folders' 'forms' 'document' 'drawings' 'presentation' 'spreadsheets' 'file')
 
-parallel --will-cite \
-	 grep $_urllist {} > {}.txt ::: $(echo "{$_types[*]}") || return
-					  
-# 4. get IDs from urls using standard url forms
-echo "For this step you need to inspect each file" && exit
-
-# 5. determine "Data Center Drive rclone" ID
-echo "Requires more manual intervention"
-
-# 6. GAM: utilize "<object_id> in <parent_id>" where parent_id is ID of "Data Center Drive rclone"
-echo "Requires experimentation"
-
+if [[ -f "$_filelist" ]]; then
+    cat "$_filelist" \
+	| $tee  "$_filelist" \
+	| grep -o "https://.*" \
+	| $tee "$_urllist" \
+	| while read -r _url; do
+	for _type in "${_types[@]}"; do
+	    echo "$_url" \
+		| grep "$_type" \
+		| $tee "${_type}".txt \
+		| case "$_type" in ### How to get the ID? We have (3) cases. Do I write a case for each?
+		document|drawings|presentation|spreadsheets)
+		    awk -F '/' '{ print $6 }' <(echo "$_url") | $tee "${_type}".id ;;
+		file|folders)
+		    awk -F '/' '{ print $4 }' <(echo "$_url") | $tee "${_type}".id ;;
+		forms)
+		    awk -F '/' '{ print $7 }' <(echo "$_url") | $tee "${_type}".id ;;
+		*)
+		    echo "${_type}: ${_url}: invalid type or malformed url, possibly unaccounted for."
+	    esac
+	done
+    done
+else
+    gam user technology show filelist \
+    	| $tee "$_filelist" \
+	| grep -o "https://.*" \
+	| $tee "$_urllist" \
+	| while read -r _url; do
+	for _type in "${_types[@]}"; do
+	    echo "$_url" \
+		| grep "$_type" \
+		| $tee "${_type}".txt \
+		| case "$_type" in ### How to get the ID? We have (3) cases. Do I write a case for each?
+		document|drawings|presentation|spreadsheets)
+		    awk -F '/' '{ print $5 }' <(echo "$_url") | $tee "${_type}".id ;;
+		file|folders)
+		    awk -F '/' '{ print $4 }' <(echo "$_url") | $tee "${_type}".id ;;
+		forms)
+		    awk -F '/' '{ print $7 }' <(echo "$_url") | $tee "${_type}".id ;;
+		*)
+		    echo "${_type}: ${_url}: invalid type or malformed url, possibly unaccounted for."
+	    esac
+	done
+    done
+fi
