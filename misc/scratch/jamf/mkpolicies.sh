ba39cd164cc9a7f90dda49c16c29b7df43943034
Elijah Beale <ebeale@protonmail.com>
2019-04-03 14:50:11 -0400

    auto commited on: 2019-04-03 18:50:11 [UTC]

--- misc/scratch/jamf/mkpolicies.sh
+++ misc/scratch/jamf/mkpolicies.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-_password='r0;tWE(rmkN8FLvzT$kD'
-_user='ebeale'
+_password=''
+_user=''
+_org=''
 _credentials="$(printf "$_user:$_password" | iconv -t ISO-8859-1 | base64 -i -)"
-_urlPrefix="https://tpschool.jamfcloud.com/JSSResource"
+_urlPrefix="https://${_org}.jamfcloud.com/JSSResource"
 _urlRequest="policies/id"
 _xmlFilePath="${HOME}/.tps/Scratch/JAMF/policyDir"
 
