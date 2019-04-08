30fbba8c54870be9de319276f35b455e50e75bb1
Elijah Beale <ebeale@protonmail.com>
2019-04-08 08:35:15 -0400

    auto commited on: 2019-04-08 12:35:15 [UTC]

--- emacs/init.el
+++ emacs/init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit org-bullets which-key use-package try atomic-chrome))))
+    (markdown-mode markdown-mode+ markdown-preview-mode magit org-bullets which-key use-package try atomic-chrome))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
