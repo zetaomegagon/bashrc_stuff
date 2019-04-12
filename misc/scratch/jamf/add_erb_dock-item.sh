7cddc73e8a67d32202b364896fdd1d8f412bfcec
ebeale <ebeale@computezero.evector>
2019-04-12 10:56:47 -0400

    auto commited on: 2019-04-12 14:56:47 [UTC]

--- misc/scratch/jamf/add_erb_dock-item.sh
+++ misc/scratch/jamf/add_erb_dock-item.sh
@@ -1,50 +1,65 @@
+#!/bin/bash -x
+
+#-- create dockutil script
+script_dir="/usr/local/TPS/scripts"
+script_name="addERBDockItem.sh"
+plist_dir="/Library/LaunchAgents"
+plist_name="org.tps.ebeale.addERBDockItem.plist"
+
+plistbuddy() { /usr/libexec/PlistBuddy "$@"; }
+
+# test for directories
+mkdir -p "$script_dir" || :
+mkdir -p "$plist_dir" || :
+
+# Create addERBDockItem.sh
+cat <<'EOF'> "$script_dir"/"$script_name"
 #!/bin/bash
 
-createERBDockScript() {
-    ## Create ERB dockutil kicker script
+script_dir="/usr/local/TPS/scripts"
+script_name="addERBDockItem.sh"
+app_name="/Applications/TPS Apps/ERBSecureBrowser.app"
+user_dir="/Users/erb"
+plist_dir="/Library/LaunchDaemons"
+plist_name="org.tps.ebeale.addERBDockItem.plist"
 
-    #-- variables
+dockutil() { /usr/local/bin/dockutil "$@"; }
 
-    #-- functions
-
-    #-- body
-}
-
-createERBDockAgent() {
-    ## Create ERB dockutil kicker for ERB docutil script
+while :; do
+  if [[ "$(who | grep console | awk '{ print $1 }')" = erb ]] && \
+     [[ -e "user_name/Library/Preferences/com.apple.dock.plist" ]]; then
     
-    #-- variables
-    local label="org.tpschool.ebeale.erb-dock-item.plist"
-    local dockutil="/usr/local/bin/dockutil"
-    local verb="--add"
-    local application="/Applications/TPS Apps/ERB Secure Browser.app"
-    local user="/Users/erb"
-    local service="/Library/LaunchAgents/$label"
-    
-    #-- functions
+    dockutil --add "$app_name" "$user_dir"
+    rm "$script_dir"/"$script_name"
+    launchctl unload -w "$plist_dir"/"$plist_name"
+    rm "$plist_dir"/"$plist_name"
+    break
 
-    # Syntactic sugar for PlistBuddy
-    plistbuddy() { /usr/libexec/PlistBuddy "$@"; }
-    
-    #-- body
+  fi
+done
+EOF
 
-    # Create LaunchAgent
-    plistbuddy -c "add :Label string $label" \
+# set script permissions and ownership
+chown root:wheel "$script_dir"/"$script_name"
+chmod +x "$script_dir"/"$script_name"
+
+
+# Create LaunchAgent
+
+plistbuddy -c "add :Label string $plist_name" \
 	       -c "add :ProgramArguments array" \
-	       -c "add :ProgramArguments:0 string $dockutil" \
-	       -c "add :ProgramArguments:1 string $verb" \
-	       -c "add :ProgramArguments:2 string $application" \
-	       -c "add :ProgramArguments:3 string $user" \
-	       -c "add :RunAtLoad array" \
+	       -c "add :ProgramArguments:0 string $script_dir/$script_name" \
 	       -c "add :RunAtLoad string true" \
-	       $service
+	       "$plist_dir"/"$plist_name"
 
-    # Set LaunchAgent Permissions
-    # and ownership
-    chown root:wheel "$service"
-    chmod 644 "$service"
+# set LaunchAgent permissions and ownership
+chown erb:staff "$plist_dir"/"$plist_name"
+chmod 644 "$plist_dir"/"$plist_name"
 
-    # Enable and start LaunchAgent
-    launchctl enable user/"$label" "$service"
-    launchctl bootstrap user "$service"
-}
+# enable and start LaunchAgent. reload if already running.
+if ! launchctl load -w "$plist_dir"/"$plist_name" >/dev/null 2>&1; then
+    launchctl unload -w "$plist_dir"/"$plist_name"
+    launchctl load -w "$plist_dir"/"$plist_name"
+else
+    launchctl load -w "$plist_dir"/"$plist_name"
+fi
