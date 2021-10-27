2ceff07f53cdff9a1dac3c9911342b44145e8ec7
Elijah Beale <elijahthomasbeale+github@gmail.com>
2021-10-26 22:28:16 -0400

    auto commited on: 2021-10-26T22:28:16,492139012-04:00

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,8 +1,15 @@
-/*** overrides ***/
+/********************/
+/** User Overrides **/
+/********************/
 
 /*** custom prefs ***/
+
 // https://www.inmotionhosting.com/support/security/dns-over-https-encrypted-sni-in-firefox/
 user_pref("network.trr.mode", 3);
-user_pref("network.security.esni.enabled", true); // enable ESNI
-user_pref("network.trr.uri", "https://www.jabber-germany.de/dns-query"); // speciry TRR resolver
+user_pref("network.trr.uri", "https://www.jabber-germany.de/dns-query");
+
+// https://blog.mozilla.org/security/2021/01/07/encrypted-client-hello-the-future-of-esni-in-firefox/
+user_pref("network.dns.echconfig.enabled", true)
+user_pref("network.dns.use_https_rr_as_altsvc", true)
+//user_pref("network.security.esni.enabled", true);
 
